a388082e784012b68e76dc1b0eb8d3772d4c7fe0
Mike Schulze <mike.schulze@garmin.com>
2016-07-26 10:06:42 -0700

    Automatically create nuget package.

--- schulzem.XamlConverters/Properties/AssemblyInfo.cs
+++ schulzem.XamlConverters/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("schulzem.XamlConverters")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle( "Xaml Converters" )]
+[assembly: AssemblyDescription( "Collection of XAML converters" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("schulzem.XamlConverters")]
+[assembly: AssemblyCompany("Mike Schulze")]
+[assembly: AssemblyProduct( "Xaml Converters" )]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

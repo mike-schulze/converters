6d0455c14059ab3565fc212b0ea81ca31bb70231
Mike Schulze <mike.schulze@live.com>
2016-07-25 20:47:53 -0700

    Added simple visibility converters.

---
+++ schulzem.XamlConverters/BooleanConverter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace schulzem.XamlConverters
+{
+    /// <summary>
+    /// Base Class for anything that converts from Boolean.
+    /// T represents class it will be converted to.
+    /// Set True to whatever you want to convert to if the source is true.
+    /// Set False to whatever you want to convert to if the source is false.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BooleanConverter<T> : IValueConverter
+    {
+        public
+        BooleanConverter
+            (
+            T aTrueValue,
+            T aFalseValue
+            )
+        {
+            True = aTrueValue;
+            False = aFalseValue;
+        }
+
+        public virtual object
+        Convert
+            (
+            object aValue, 
+            Type aTargetType, 
+            object aParameter, 
+            CultureInfo aCulture
+            )
+        {
+            return aValue is bool && ((bool)aValue) ? True : False;
+        }
+
+        public virtual object
+        ConvertBack
+            (
+            object aValue, 
+            Type aTargetType, 
+            object aParameter, 
+            CultureInfo aCulture
+            )
+        {
+            return aValue is T &&
+                   EqualityComparer<T>.Default.Equals( (T) aValue, True );
+        }
+
+        public T True { get; set; }
+        public T False { get; set; }
+    }
+
+    /// <summary>
+    /// Converts from Bool to Visisbility.
+    /// Can be re-used for the inverse. And to convert to Hidden or Collapsed.
+    /// </summary>
+    [ValueConversion( typeof( object ), typeof( Visibility ) )]
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter
+            (
+            ) 
+        :
+            base( Visibility.Visible, Visibility.Collapsed )
+        { 
+        }
+    }
+
+    /// <summary>
+    /// Check if string is empty. Can be re-used for inverse.
+    /// </summary>
+    public sealed class StringEmptyToBoolConverter : BooleanConverter<bool>
+    {
+        public StringEmptyToBoolConverter
+            (
+            )
+        :
+            base( true, false )
+        {
+        }
+
+        public override object
+        Convert
+            (
+            object aValue,
+            Type aTargetType,
+            object aParameter,
+            CultureInfo aCulture
+            )
+        {
+            if( !(aValue is string) )
+            {
+                // if not a string, return that it IS empty
+                return True;
+            }
+
+            return String.IsNullOrEmpty( ( string )aValue ) ? True : False;
+        }
+    }
+
+    /// <summary>
+    /// Simply inverts a bool.
+    /// </summary>
+    public sealed class InverseBooleanConverter : BooleanConverter<bool>
+    {
+        public InverseBooleanConverter
+            (
+            )
+        :
+            base( false, true )
+        {
+        }
+    }
+}
---
+++ schulzem.XamlConverters/NullConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace schulzem.XamlConverters
+{
+[ValueConversion( typeof( object ), typeof( Visibility ) )]
+public class NullToVisibilityConverter : IValueConverter
+{
+    public object
+    Convert
+        (
+        object aValue,
+        Type aTargetType,
+        object aParameter,
+        CultureInfo aCulture
+        )
+    {
+        return aValue == null ? Null : NotNull;
+    }
+
+    public object
+    ConvertBack
+        (
+        object aValue,
+        Type aTargetType,
+        object aParameter,
+        CultureInfo aCulture
+        )
+    {
+        throw new NotSupportedException();
+    }
+
+    public Visibility Null{ get; set; }
+    public Visibility NotNull{ get; set; }
+}
+
+}
